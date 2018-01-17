0a5d4203c7b690a1477027adc8b6af6f14484dab
Justin Drew <jdrew@sca-corp.com>
2018-01-16 18:56:24 -0700

    fix for checking if email in use on signup

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,4 +1,4 @@
 class Api::UsersController < ApplicationController
   # user won't be valid yet if creating new account
-  skip_before_action :validate_current_user, :only => [:create]
+  before_action :validate_current_user, :except => [:create, :show]
 end
